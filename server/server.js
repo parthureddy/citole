0b220149a3f66f3369b707216892606ee9b09658
parthureddy <nookaparthu@gmail.com>
2020-06-04 22:45:54 +0530

    server changes

--- server/server.js
+++ server/server.js
@@ -15,13 +15,15 @@ const async = require('async');
 require('dotenv').config();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE,{useNewUrlParser: true,useUnifiedTopology: true,
+mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser: true,useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false})
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+app.use(express.static('client/build'))
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
@@ -547,13 +549,13 @@ app.post('/api/site/site_data',auth,admin,(req,res)=>{
     )
 })
 
-// DEFAULT 
-// if( process.env.NODE_ENV === 'production' ){
-//     const path = require('path');
-//     app.get('/*',(req,res)=>{
-//         res.sendfile(path.resolve(__dirname,'../client','build','index.html'))
-//     })
-// }
+DEFAULT 
+if( process.env.NODE_ENV === 'production' ){
+    const path = require('path');
+    app.get('/*',(req,res)=>{
+        res.sendfile(path.resolve(__dirname,'../client','build','index.html'))
+    })
+}
 
 
 const port = process.env.PORT || 3002;
